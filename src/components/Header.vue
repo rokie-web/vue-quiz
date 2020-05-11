7bc3dfb4e6439b378fd76f3767b00d942cb6022e
Robert Kiemele <robert.kiemele@bexio.com>
2020-05-11 15:47:43 +0200

    Adding bootstrap and some first compoents

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item active>Fancy Quiz App</b-nav-item>
+            <b-nav-item>Counter: 0/0</b-nav-item>
+        </b-nav>
+    </div>
+</template>
